99e16f46d1e6edc2a29db1109f3c0eb4469c5963
Nikola Jovanovic <nikola@madebymane.com>
2021-02-02 12:34:12 +0100

    add range slider

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,3 +1,18 @@
 $( document ).ready(function() {
     console.log( "Polećemooo!" );
+
+    $("#slider-range").slider({
+        range: true, 
+        min: 25,
+        max: 220,
+        step: 1,
+        slide: function( event, ui ) {
+          $( "#min-price").html(ui.values[ 0 ]);
+          
+          $( "#max-price").html(ui.values[ 1 ]);         
+        }
+      })
 });
+
+
+  
